9a2ac848748155fb0a3cedc343d2272f6c9b7eca
vanessa middleton <ness.middleton@gmail.com>
2021-02-24 19:56:16 -0800

    Added unit test passing

---
+++ VMidd_PlanCheckerTests/ViewModels/CTDateConstraintTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ESAPX_StarterUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESAPIX.Constraints;
+
+namespace ESAPX_StarterUI.ViewModels.Tests
+{
+    [TestClass()]
+    public class CTDateConstraintTests
+    {
+        [TestMethod()]
+        public void IsCTTooOldTest_FAILS_CORRECTLY()
+        {
+            //Arrange
+            var daysOld = 61;
+
+            //Act
+            var actual = new CTDateConstraint().IsCTTooOld(daysOld).ResultType;
+
+            //Assert
+            var expected = ResultType.ACTION_LEVEL_3;
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void IsCTTooOldTest_PASSES_CORRECTLY()
+        {
+            //Arrange
+            var daysOld = 59;
+
+            //Act
+            var actual = new CTDateConstraint().IsCTTooOld(daysOld).ResultType;
+
+            //Assert
+            var expected = ResultType.PASSED;
+            Assert.AreEqual(expected, actual);
+        }
+     }
+}
